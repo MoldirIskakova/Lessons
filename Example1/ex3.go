568d3dcbaeecea585ff3775e45efb8f22601ec83
Monika <Moldir_isb@bk.ru>
2022-12-08 22:58:07 +0600

    Даны стороны прямоугольника a и b. Найти его площадь S = a∗b и периметр P=2∗(a+b).

---
+++ Example1/ex3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := 7
+	S := a * b
+	P := 2 * (a + b)
+
+	fmt.Println("Ploshad' primougol'nika:", S)
+	fmt.Println("Perimetr primougol'nika:", P)
+
+}
